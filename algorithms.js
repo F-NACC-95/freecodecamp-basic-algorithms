236bd9fbace4f02d41fcca1837f038906c80dfe1
Franco Naccarato <f-nacc-21@hotmail.com>
2019-04-08 09:42:39 -0400

    second half of questions and answers

--- algorithms.js
+++ algorithms.js
@@ -219,4 +219,247 @@ truncateString("A-tisket a-tasket A green and yellow basket", 8);
 
 
 
+------------------------------ QUESTION 9
+
+Basic Algorithm Scripting: Finders Keepers
+
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+
+
+
+
+function findElement(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    if ( func(arr[i]) ) {
+      return arr[i]
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
+
+
+
+
+
+------------------------------ QUESTION 10
+
+Basic Algorithm Scripting: Boo who
+Check if a value is classified as a boolean primitive. Return true or false.
+
+Boolean primitives are true and false.
+
+Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+
+
+
+
+function booWho(bool) {
+  return typeof bool === 'boolean'
+}
+
+booWho(null);
+
+
+
+
+
+------------------------------ QUESTION 11
+
+Basic Algorithm Scripting: Title Case a Sentence
+
+Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+
+
+
+
+function titleCase(str) {
+  let wordsArr = str.toLowerCase().split(' ');
+
+  let wordsMap = wordsArr.map(word => {
+    return word.replace( word.charAt(0), word.charAt(0).toUpperCase() );
+  })
+  return wordsMap.join(' ')
+}
+
+titleCase("I'm a little tea pot");
+
+
+
+
+
+------------------------------ QUESTION 12
+
+Basic Algorithm Scripting: Slice and Splice
+
+You are given two arrays and an index.
+
+Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+Begin inserting elements at index n of the second array.
+
+Return the resulting array. The input arrays should remain the same after the function runs.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+
+
+
+
+function frankenSplice(arr1, arr2, n) {
+  let newArr = arr2.slice(0, )
+  newArr.splice(n, 0, ...arr1);
+  return newArr
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
+
+
+
+
+
+------------------------------ QUESTION 13
+
+Basic Algorithm Scripting: Falsy Bouncer
+
+Remove all falsy values from an array.
+
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+Hint: Try converting each value to a Boolean.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+
+
+
+
+function bouncer(arr) {
+  return arr.filter(Boolean);
+}
+
+bouncer([7, "ate", "", false, 9]);
+
+
+
+
+
+------------------------------ QUESTION 14
+
+Basic Algorithm Scripting: Where do I Belong
+
+Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+For example, getIndexToIns([1,2,3,4], 1.5) should return 1 because it is greater than 1 (index 0), but less than 2 (index 1).
+
+Likewise, getIndexToIns([20,3,5], 19) should return 2 because once the array has been sorted it will look like [3,5,20] and 19 is less than 20 (index 2) and greater than 5 (index 1).
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+
+
+
+
+function getIndexToIns(arr, num) {
+  let newArr = arr.slice(0, );
+  newArr.push(num);
+
+  newArr.sort( (a, b) => {
+    return a - b;
+  })
+
+  return newArr.indexOf(num)
+}
+
+getIndexToIns([40, 60], 50);
+
+
+
+
+
+------------------------------ QUESTION 15
+
+Basic Algorithm Scripting: Mutations
+
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+
+
+
+
+function mutation(arr) {
+  let firstElement = arr[0].toLowerCase();
+  let secondElement = arr[1].toLowerCase();
+
+  for (let i = 0; i < secondElement.length; i++) {
+    if (firstElement.indexOf(secondElement[i]) < 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
+
+
+
+
+
+------------------------------ QUESTION 16
+
+Basic Algorithm Scripting: Chunky Monkey
+
+Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+
+
+
+
+function chunkArrayInGroups(arr, size) {
+  let tempArr = [];
+  let chunkedArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (i % size !== size - 1) {
+      tempArr.push(arr[i])
+    } else {
+      tempArr.push(arr[i]);
+      chunkedArr.push(tempArr);
+      tempArr = [];
+    }
+  }
+
+  if (tempArr.length > 0) {
+    chunkedArr.push(tempArr)
+  }
+  
+  return chunkedArr
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
+
+
+
+
+
 */
